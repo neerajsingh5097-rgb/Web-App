0f92a2273ad5417b8fcbfc9ae0a57ddfe3abfe59
Neeraj Singh <neerajsingh5097@gmail.com>
2026-03-21 16:49:17 +0530

    update

--- src/main/java/com/spring/mvc/controller/UserController.java
+++ src/main/java/com/spring/mvc/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.spring.mvc.controller;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,6 +16,21 @@ import com.spring.mvc.model.Employee;
 
 @Controller
 public class UserController {
+	
+	static Map<Integer,Employee> map = null;
+	static int empId=0;
+
+	static {
+		map= new HashMap<>();
+		map.put(++empId,new Employee(empId, "Mukesh","IT","Noida"));
+		map.put(++empId,new Employee(empId, "Nitesh","IT","Noida"));
+		map.put(++empId,new Employee(empId, "Abhishek","IOT","Noida"));
+		map.put(++empId,new Employee(empId, "Nakul","AT","Noida"));
+		map.put(++empId,new Employee(empId, "Prakash","IT","Noida"));
+
+
+
+	}
 	
 	@GetMapping("/")
 	public String welcome()
@@ -26,7 +46,7 @@ public class UserController {
 		return "sign-up";
 	}
 	
-	@PostMapping("/signupUser")
+	@GetMapping("/signupUser")
 	public String postUser(@RequestParam(name =  "username") String username,@RequestParam(name="department")String department,@RequestParam(name="address")String address,Model model)
 	{
 		System.out.println("Username : "+username);
@@ -43,10 +63,46 @@ public class UserController {
 	
 	
 	@PostMapping("/createUser")
-	public String createUser(@ModelAttribute Employee employee)
+	public String createUser(@ModelAttribute Employee employee,Model model)
 	{
 		System.out.println(employee);
-		return "index";
+		employee.setId(++empId);
+		map.put(empId, employee);
+		model.addAttribute("employee",employee);
+		return "success";
 	}
+	
+
+	@GetMapping("/getEmpById/{id}")
+	public String getEmp(@PathVariable (name = "id") int id,Model model)
+	{
+
+		Employee e= map.get(id);
+		
+		model.addAttribute("employee", e);
+		
+		return "success";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
